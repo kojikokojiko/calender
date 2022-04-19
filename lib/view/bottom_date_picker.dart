e7d3e297a368d024345a915bf99bfc0f23566519
CSP2020-NOTE4\iwase <kojiko430430@gmail.com>
2022-04-20 06:05:47 +0900

    cupertinodatepickerべた書き

---
+++ lib/view/bottom_date_picker.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+Widget _bottomPicker(Widget picker) {
+  return Container(
+    height: 216,
+    padding: const EdgeInsets.only(top: 6.0),
+    color: CupertinoColors.white,
+    child: DefaultTextStyle(
+      style: const TextStyle(
+        color: CupertinoColors.black,
+        fontSize: 22.0,
+      ),
+      child: GestureDetector(
+        onTap: () {},
+        child: SafeArea(
+          top: false,
+          child: picker,
+        ),
+      ),
+    ),
+  );
+}
--- lib/view/mordal_slider.dart
+++ lib/view/mordal_slider.dart
@@ -1,54 +1,72 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
-
+import 'package:intl/intl.dart';
+import 'add_schedule_page.dart';
 class modalSlider extends StatelessWidget {
   modalSlider({this.day});
+
   final DateTime? day;
-  final diff_day=2;
+  final diff_day = 2;
 
+  List<DateTime> day_list = [];
 
-  List<DateTime> day_list=[];
-
-
-  void setdaylist(diffDay,day){
+  void setdaylist(diffDay, day) {
     day_list.clear();
-    for (int i =diffDay;i>0;i--){
-      var insertday=day!.add(Duration(days: i)*-1);
+    for (int i = diffDay; i > 0; i--) {
+      var insertday = day!.add(Duration(days: i) * -1);
       day_list.add(insertday);
     }
     day_list.add(day!);
-    for (int i =1;i<diffDay+1;i++){
-      var insertday=day!.add(Duration(days: i));
+    for (int i = 1; i < diffDay + 1; i++) {
+      var insertday = day!.add(Duration(days: i));
       day_list.add(insertday);
     }
   }
-
-
-
 
   @override
   Widget build(BuildContext context) {
     setdaylist(diff_day, day);
     return CarouselSlider(
       options: CarouselOptions(
-          aspectRatio: 2.0,
+        aspectRatio: 2.0,
         height: 550,
         viewportFraction: 0.8,
-          initialPage:diff_day,
+        initialPage: diff_day,
       ),
-      items: day_list.map((i) {
+      items: day_list.map((selecting_day) {
         return Builder(
           builder: (BuildContext context) {
             return Container(
               decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(10),
-                color: Colors.white
-              ),
-              
-              margin: EdgeInsets.only(bottom: 40,left:10,right: 10),
+                  borderRadius: BorderRadius.circular(15), color: Colors.white),
+
+              margin: EdgeInsets.only(bottom: 40, left: 10, right: 10),
               // color: Colors.white,
-              child: Center(
-                child: Text('text $i', style: TextStyle(fontSize: 16.0),),
+              child: Container(
+                padding: EdgeInsets.all(10),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        // Text(day.toString()),
+                        Text(DateFormat("yyyy/MM/dd (E)").format(selecting_day),
+                            style: TextStyle(fontSize: 20)),
+                        TextButton(
+                            child: Icon(Icons.add),
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => AddSchedulePage(),
+                                  ));
+                            }),
+                      ],
+                    ),
+                    Divider(color: Colors.black),
+                    Center(child: Text("予定はありません")),
+                  ],
+                ),
               ),
             );
           },
